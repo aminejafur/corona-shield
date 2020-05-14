46cfaf0ddcf26e38daf5e768f61b20f7f51c6f22
unknown <amine.jafur@gmail.com>
2020-05-14 04:31:09 +0000

    Update Dashboard Default Text

--- Server/dashboard/assets/js/default.js
+++ Server/dashboard/assets/js/default.js
@@ -7,7 +7,7 @@ export default function ShowDefault(){
 			<span>
 			<h3> Welcome, </h3> 
 			<h3 style="font-size:15px;padding:5px" >
-			<p style="font-weight: 100;" >Morocco Corona Shield is an app that ..., consectetur adipiscing elit. Fusce non turpis pretium, finibus nibh eget, dictum nisl. Aliquam bibendum vitae nunc ac consequat. Cras aliquam volutpat eros, vel faucibus nibh bibendum nec.</p> </h3> 
+			<p style="font-weight: 100;" >Corona Shield is an open sourced Corona Tracking Mobile Application with Back-end made by Amine Jafur using Flutter, PHP, JS, <br><a style="color:#7a90fd" href="https://github.com/aminejafur/corona-shield" target="_blank">Read More</a></p> </h3> 
 			</span>
 		</div>`;
 }
